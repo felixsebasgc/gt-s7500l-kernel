25086d7ed82a3a2bd30e78d08b24ceb03d777162
guillermo <guillermo.aguiecha@gmail.com>
2013-05-02 13:16:58 -0600

    Initial commit

--- build_final.sh
+++ build_final.sh
@@ -1,3 +1,3 @@
-cp GT-S7500L/arch/arm/boot/Image Image
+cp GT-S7500L-Kernel/arch/arm/boot/Image Image
 ./mkbootfs initramfs | gzip > ramdisk.gz
 ./mkbootimg --kernel Image --ramdisk ramdisk.gz --cmdline "console=ttyHSL0,115200,n8 androidboot.hardware=qcom" --base 0x00200000 --pagesize 2048 -o boot.img
